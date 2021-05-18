6d9c1405a3a33b51f275c56a580d804d5969be12
Benjamin Dehlin <ben.dehlin@gmail.com>
2021-05-18 13:32:27 -0600

    hooks lecture

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,27 @@
+// import {useState, useEffect} from 'react'
+// import axios from 'axios'
+import Poke from './Poke'
+import useAxios from '../hooks/useAxios'
+
 const List = () => {
+  const [pokemon, setPokemon] = useAxios('https://pokeapi.co/api/v2/pokemon/', 'results')
+  console.log(pokemon)
+  // const [pokemon, setPokemon] = useState([])
+
+  // useEffect(() => {
+  //   axios.get('https://pokeapi.co/api/v2/pokemon/')
+  //   .then(res => setPokemon(res.data.results))
+  //   .catch(err => console.log(err))
+  // }, [])
+  
   return(
     <div>
       <h1>List</h1>
+      {pokemon.map((poke) => {
+        return(
+          <Poke key={poke.name} poke={poke} />
+        )
+      })}
     </div>
   )
 }
---
+++ src/components/Pokemon.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Pokemon = (props) => {
+  const [pokeInfo, setPokeInfo] = useState(null)
+  const {name} = props.match.params
+
+  useEffect(() => {
+    console.log(props)
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((res) => {
+      setPokeInfo(res.data)
+    }).catch((err) => console.log(err))
+  }, [name])
+
+  console.log(pokeInfo)
+  return(
+    <div>
+      <h1>Pokemon Page</h1>
+      {pokeInfo ? (
+        <div>
+          <p>{pokeInfo.name}</p>
+          <img src={pokeInfo.sprites.front_default} />
+        </div>
+      ):(
+        <div>...Loading</div>
+      )}
+    </div>
+  )
+}
+
+export default Pokemon
